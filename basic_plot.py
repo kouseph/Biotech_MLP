f5abe59e50fb245b60d1bcf7ddbe9f6a9c7d043e
kouseph <kouseph@g.ucla.edu>
2026-02-25 12:33:44 -0800

    Added basic plotting graph. Looks very messy and cannot draw conclusions from it currently

---
+++ basic_plot.py
@@ -0,0 +1,52 @@
+import requests
+import matplotlib.pyplot as plt
+from datetime import datetime
+import finnhub 
+
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv() 
+
+finnhub_api_key = os.getenv("FINNHUB_API_KEY")
+
+
+def plot_finnhub_library_data(symbol):
+    # Initialize the Finnhub client
+    finnhub_client = finnhub.Client(api_key=finnhub_api_key)
+    
+    financials = finnhub_client.company_basic_financials(symbol, 'all')
+    
+    series_data = financials.get('series', {}).get('annual', {}).get('currentRatio', [])
+    
+    if not series_data:
+        print(f"No Current Ratio data available for {symbol}.")
+        return
+
+    series_data.sort(key=lambda x: datetime.strptime(x['period'], '%Y-%m-%d'))
+
+    dates = [item['period'] for item in series_data]
+    values = [item['v'] for item in series_data]
+
+    # Create the Plot
+    return [dates, values, symbol]
+
+
+VanEck = ["LLY", "NVS", "MRK", "NVO", "GSK", "BMY", "JNJ", "MCK", "PFE", "AZN", "ABBV", "COR", "SNY", "ZTS", "HLN", "TAK", "TEVA", "VTRS", "JAZZ", "AXSM", "ELAN", "CORT", "OGN", "BHC", "PRGO"]
+
+ans = []
+for i in VanEck:
+    ans.append(plot_finnhub_library_data(i))
+
+plt.title(f'Annual Current Ratio (via Finnhub Library)')
+plt.figure(figsize=(10, 5))
+for i in ans:
+    plt.plot(i[0], i[1], marker='s', label=i[2])
+plt.xlabel('Fiscal Period')
+plt.ylabel('Value (v)')
+plt.grid(axis='y', linestyle='--', alpha=0.6)
+plt.legend()
+
+
+plt.show()
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 import finnhub
+import pandas as pd
 
 load_dotenv() 
 
@@ -8,7 +9,63 @@ finnhub_api_key = os.getenv("FINNHUB_API_KEY")
 
 finnhub_client = finnhub.Client(api_key=finnhub_api_key)
 
-print(finnhub_client.company_basic_financials('JNJ', 'all'))
+VanEck = ["LLY", "NVS", "MRK", "NVO", "GSK", "BMY", "JNJ", "MCK", "PFE", "AZN", "ABBV", "COR", "SNY", "ZTS", "HLN", "TAK", "TEVA", "VTRS", "JAZZ", "AXSM", "ELAN", "CORT", "OGN", "BHC", "PRGO"]
+
+# df = finnhub_client.company_basic_financials('JNJ', 'all')
+# print(pd.read_json('hey.json'))
 
 
+data = finnhub_client.company_basic_financials('IHE', 'all')
+
+data.get(series_data = data.get('series', {}).get('annual', {}).get('currentRatio', []))
+
+
+# pd.read_json(finnhub_client.company_basic_financials('IHE', 'all'))
+
+
+# for i in VanEck:
+#     data.append(finnhub_client.company_basic_financials(i, 'metrics'))
+
+
+
+"""
+PPH	VanEck Pharmaceutical ETF
+IHE	iShares U.S. Pharmaceuticals ETF
+PJP	Invesco Pharmaceuticals ETF	
+XPH	State Street SPDR S&P Pharmaceuticals ETF
+KURE	KraneShares MSCI All China Health Care Index ETF	
+FTXH	First Trust Nasdaq Pharmaceuticals ETF
+PILL	Direxion Daily Pharmaceutical & Medical Bull 3X Shares
+"""
+
+
+"""
+{
+        'metric':
+        {'10DayAverageTradingVolume': 0.07472, 
+         '13WeekPriceReturnDaily': 11.2509,
+         '26WeekPriceReturnDaily': 31.036,
+         '3MonthADReturnStd': 16.401266,
+         '3MonthAverageTradingVolume': 0.09736,
+         '52WeekHigh': 91.49,
+         '52WeekHighDate': '2026-02-13',
+         '52WeekLow': 58.9737,
+         '52WeekLowDate': '2025-04-09',
+         '52WeekPriceReturnDaily': 27.3083,
+         '5DayPriceReturnDaily': 0.6966,
+         'beta': 0.5207113,
+         'monthToDatePriceReturnDaily': 5.0646,
+         'priceRelativeToS&P50013Week': 7.871,
+         'priceRelativeToS&P50026Week': 24.9595,
+         'priceRelativeToS&P5004Week': 6.4361,
+         'priceRelativeToS&P50052Week': 13.5653,
+         'priceRelativeToS&P500Ytd': 7.287,
+         'yearToDatePriceReturnDaily': 7.3559},
+
+        'metricType': 'all', 
+        'series': {}, 
+        'symbol': 'IHE'}
+
+
+"""
 
